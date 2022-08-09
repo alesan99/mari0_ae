08de09385c776e05a801158d3c7e90f355c45f4e
Aidan <71894636+Aid0nModder@users.noreply.github.com>
2022-08-09 17:53:26 +0100

    Glados refactor.
    Figured she deserved it, no other entity comes close to how poor hers was

--- enemies/glados.lua
+++ enemies/glados.lua
@@ -27,47 +27,34 @@ function glados:init(x, y, r)
 	--IMAGE STUFF
 	self.drawable = true
 	self.graphic = gladosimage
-	self.quad = gladosquad[1]
+	self.quad = gladosquad[1] -- I have two sides, glados and .
 	self.offsetX = 32
 	self.offsetY = -26
 	self.quadcenterX = 34
 	self.quadcenterY = 34
 	
 	self.rotation = 0 --for portals
-
 	self.animationdirection = "left"
-	
-	self.timer = 0
-	self.explodetimer = 0
-	self.pooftimer = 0
-	
+
 	self.hp = 4
-	self.rp = 4
-	self.coredetect = false
-	self.dead = false
-	self.boot = false
-	self.startcore = false
-	self.initiated = false
-	self.playglados1sound = true
+	self.state = "notseen"
+	self.targetcore = false
+	self.explodetimer = 0
 end
 
 function glados:update(dt)
-	if self.boot and not self.dead then
-		if self.playglados1sound == true then
-			playsound(glados1sound)
-			self.playglados1sound = false
+	-- she may not be used but at least she's clean
+	if self.state == "notseen" then
+		if not onscreen(self.x, self.y, self.width, self.height) then
+			return
 		end
-		
-		if self.timer < 4 then
-			self.timer = self.timer + dt
-			if self.timer > 4 then
-				table.insert(objects["core"], core:new(self.x+32/16, self.y+60/16, "morality"))
-				self.initiated = true
-				self.startcore = true
-				self.hp = 3
-			end
-		end
-		closestplayer = 1 --find closest player
+		playsound(glados1sound)
+		self.state = "start"
+	end
+
+	if self.state ~= "dead" then
+		-- follow closest player
+		closestplayer = 1
 		for i = 2, players do
 			local v = objects["player"][i]
 			if math.abs(self.x - v.x) < math.abs(self.x+30/16 - objects["player"][closestplayer].x) then
@@ -82,120 +69,83 @@ function glados:update(dt)
 			self.animationdirection = "left"
 			self.dir = "left"
 		end
-		
-		if self.rp < 4 and self.rp ~= 0 then
-			if not neurotoxin then
-				neurotoxin = true
-				toxintime = self.toxintime
-			end
-		end
-		
-		if self.startcore == true then
-			if #objects["core"] == 0 then
-				self.hp = 3
-				self.rp = 3
-				self.startcore = false
-				playsound(boomsound)
-				playsound(glados2sound)
-			end
-		end
-		
-		if self.coredetect == true then
-			if #objects["core"] == 0 then
-				self.rp = self.rp - 1
-				self.coredetect = false
-				playsound(boomsound)
-			end
-		end
-		if self.rp == 0 then
-			self.dead = true
-		end
-	elseif self.dead then
-		neurotoxin = false
-		self.explodetimer = self.explodetimer + dt
-		earthquake = 20
-		if self.explodetimer > 4 then
-			nextlevel()
-			return true--EXPLODE
-		else
-			if boomsound:isStopped() then
-				playsound(boomsound)
-			end
-			self.pooftimer = self.pooftimer - dt
-			if self.pooftimer < 0 then
-				makepoof(self.x+self.width*math.random(), self.y+self.height*math.random(), "poof")
-				makepoof(self.x+self.width*math.random(), self.y+self.height*math.random(), "poof")
-				self.pooftimer = 0.2
-			end
-		end
-	else
-		if self.x < xscroll+25 then
-			self.boot = true
-		end
 	end
-end
 
-function glados:hit()
-	if self.dead then
-		return false
-	end
-	if self.hp == self.rp and self.initiated then
-		self.hp = self.hp - 1
-		
-		if self.hp == 2 then
-			table.insert(objects["core"], core:new(self.x+32/16, self.y+60/16, "curiosity"))
-		elseif self.hp == 1 then
-			table.insert(objects["core"], core:new(self.x+32/16, self.y+60/16, "cakemix"))
-		elseif self.hp == 0 then --still alive
-			table.insert(objects["core"], core:new(self.x+32/16, self.y+60/16, "anger"))
+	if self.state == "start" then
+		if glados1sound:isStopped() then
+			self:spawncore(self.hp)
+			self.state = "running"
 		end
-		self.coredetect = true
+	elseif self.state == "running" then
+		if self.targetcore and self.targetcore.destroying then
+			self.targetcore = false
+			self.hp = self.hp - 1
+			playsound(boomsound)
+			if self.hp == 3 then -- first core
+				playsound(glados2sound)
+				toxintime = self.toxintime
+				neurotoxin = true
+			elseif self.hp == 0 then -- last core
+				self.state = "dead"
+				neurotoxin = false
+				glados2sound:stop()
+			end
+		end
 	end
 	
-	playsound(boomsound)
-end
-
-function glados:leftcollide(a, b)
-	if self:globalcollide(a, b) then
-		return false
+	if self.state ~= "dead" then
+		return
 	end
-	return false
-end
-
-function glados:rightcollide(a, b)
-	if self:globalcollide(a, b) then
-		return false
-	end
-	return false
-end
-
-function glados:ceilcollide(a, b)
-	if self:globalcollide(a, b) then
-		return false
+	self.explodetimer = self.explodetimer + dt
+	if self.explodetimer > 4 then
+		nextlevel()
+		return true
+	elseif boomsound:isStopped() then
+		earthquake = 20
+		playsound(boomsound)
+		makepoof(self.x+self.width*math.random(), self.y+self.height*math.random(), "poof")
+		makepoof(self.x+self.width*math.random(), self.y+self.height*math.random(), "poof")
 	end
 end
 
 function glados:globalcollide(a, b)
 	if a == "turretrocket" then
 		self:hit()
-	elseif a == "bulletbill" or a == "cannonball" or a == "bigbill" or a == "torpedoted" then
-		if b.killstuff then
-			b:shotted(self.dir)
-			self:hit()
-		end
+	elseif (a == "bulletbill" or a == "cannonball" or a == "bigbill" or a == "torpedoted") and b.killstuff then
+		b:shotted(self.dir)
+		self:hit()
 	elseif a == "mariohammer" then
 		b:explode()
 		self:hit()
 	end
 end
 
-function glados:floorcollide(a, b)
-	if self:globalcollide(a, b) then
+function glados:hit()
+	if self.state == "dead" or self.targetcore then
 		return false
 	end
+	self:spawncore(self.hp)
+	playsound(boomsound)
 end
 
+function glados:spawncore(i)
+	local cores = {"anger","cakemix","curiosity","morality"}
+	self.targetcore = core:new(self.x+32/16, self.y+60/16, cores[i])
+	table.insert(objects["core"], self.targetcore)
+end
+
+function glados:leftcollide(a, b)
+	self:globalcollide(a, b)
+end
+function glados:rightcollide(a, b)
+	self:globalcollide(a, b)
+end
+function glados:ceilcollide(a, b)
+	self:globalcollide(a, b)
+end
+function glados:floorcollide(a, b)
+	self:globalcollide(a, b)
+end
 function glados:passivecollide(a, b)
 	self:leftcollide(a, b)
-	return false
 end
