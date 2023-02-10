888b4172d890afeae5080f9d3666d94176062a31
alesan99 <alesanesp99@gmail.com>
2023-02-10 08:42:41 -0600

    Fix collectable lock not turning back on Update Version number

--- main.lua
+++ main.lua
@@ -55,8 +55,8 @@ if debugconsole then debuginputon = true; debuginput = "print()"; print("DEBUG O
 local debugGraph,fpsGraph,memGraph,drawGraph
 local debugGraphs = false
 
-VERSION = 13.0124
-VERSIONSTRING = "13.1 (2/6/23)"
+VERSION = 13.1
+VERSIONSTRING = "13.1 (2/10/23)"
 
 android = (love.system.getOS() == "Android" or love.system.getOS() == "iOS") --[DROID]
 androidtest = false--testing android on pc
