12634591df6c03415d50acbe3db63baf8a9f6ae0
Lexi Larkin <lexi@qixils.dev>
2023-03-04 19:38:20 -0500

    feat: android build workflow

--- .scripts/extract-metadata.py
+++ .scripts/extract-metadata.py
@@ -32,11 +32,17 @@ with open(toml_path, 'r') as toml_file:
     metadata['id'] = re.search(r'^name\s*=\s*"(.+)"', toml).group(1)
     # ensure title is the same as the one in main.lua
     # TODO: don't assert, just print a GH Actions warning (idk how)
-    if metadata['title'] != re.search(r'^ProductName\s*=\s*"(.+)"$', toml, re.MULTILINE).group(1):
+    if (_win_title := re.search(r'^ProductName\s*=\s*"(.+)"$', toml, re.MULTILINE)) is None:
+        print('::error file=makelove.toml::Windows product name could not be found')
+    if metadata['title'] != _win_title.group(1):
         print('::error file=makelove.toml::Windows product name does not match game title in main.lua')
-    if metadata['title'] != re.search(r'^[linux.desktop_file_metadata]\r?\nName\s*=\s*"(.+)"$', toml, re.MULTILINE).group(1):
+    if (_linux_title := re.search(r'\[linux.desktop_file_metadata\]\n+Name\s*=\s*"(.+)"', toml, re.MULTILINE)) is None:
+        print('::error file=makelove.toml::Linux desktop file name could not be found')
+    if metadata['title'] != _linux_title.group(1):
         print('::error file=makelove.toml::Linux desktop file name does not match game title in main.lua')
-    if metadata['title'] != re.search(r'^CFBundleDisplayName\s*=\s*"(.+)"$', toml, re.MULTILINE).group(1):
+    if (_mac_title := re.search(r'^CFBundleDisplayName\s*=\s*"(.+)"$', toml, re.MULTILINE)) is None:
+        print('::error file=makelove.toml::macOS bundle display name could not be found')
+    if metadata['title'] != _mac_title.group(1):
         print('::error file=makelove.toml::macOS bundle display name does not match game title in main.lua')
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as env_file:
